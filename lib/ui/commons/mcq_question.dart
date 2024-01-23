d2fbb203c47b964bffe7891b6d39435ebcf09562
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-01-23 14:23:04 +0530

    Added colors themewise

--- lib/ui/commons/mcq_question.dart
+++ lib/ui/commons/mcq_question.dart
@@ -12,7 +12,7 @@ class _McqQuestionState extends State<McqQuestion> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.only(top: 20),
       children: [
         Container(
           height: 200,
@@ -23,8 +23,8 @@ class _McqQuestionState extends State<McqQuestion> {
           child: Column(
             children: [
               Transform.translate(
-                offset: Offset(0, -18),
-                child: Stack(
+                offset: const Offset(0, -18),
+                child: const Stack(
                   alignment: Alignment.center,
                   children: [
                     CircularProgressIndicator(
@@ -35,20 +35,20 @@ class _McqQuestionState extends State<McqQuestion> {
                   ],
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 50,
               ),
-              Text("What is the most popular sports in history?")
+              const Text("What is the most popular sports in history?")
             ],
           ),
         ),
-        SizedBox(
+        const SizedBox(
           height: 20,
         ),
-        OptionCard(title: "Football", isSelected: false, isFalse: false),
-        OptionCard(title: "Cricket", isSelected: true, isFalse: false),
-        OptionCard(title: "Basketball", isSelected: false, isFalse: true),
-        OptionCard(title: "Tennis", isSelected: false, isFalse: false),
+        const OptionCard(title: "Football", isSelected: false, isFalse: false),
+        const OptionCard(title: "Cricket", isSelected: true, isFalse: false),
+        const OptionCard(title: "Basketball", isSelected: false, isFalse: true),
+        const OptionCard(title: "Tennis", isSelected: false, isFalse: false),
       ],
     );
   }
---
+++ lib/utils/styles/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  //LIGHT THEME
+  static const Color bgWhite = Color(0xFFFAFAFA);
+  static const Color fontBlack = Color(0xFF1C1C1C);
+  static const Color fontGrey = Color(0xFF737373);
+  static const Color blueFont = Color(0xFF3EBDD4);
+  static const Color blueBg = Color(0xFFdcf8ff);
+  static const Color orange = Color(0xFFff9f41);
+  static const Color borderGrey = Color(0xFFefefef);
+  static const Color red = Color(0xFFFF5341);
+  static const Color redBg = Color(0xFFffdfdc);
+
+  //DARK THEME
+  static const Color bgBlack = Color(0xFF1C1C1C);
+  static const Color fontWhite = Color(0xFFFFFFFF);
+  static const Color greyBg = Color(0xFF323232);
+}
