0ad3af6afc459bfffa1550f7fa2da62a7ddc7611
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2024-02-02 01:52:22 +0530

    Updated initial route. Fixed back gesture closing issue

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -13,7 +13,8 @@ class Routes {
   static const String quizEndScreen = '/quiz_end_screen';
 
   static Map<String, Widget Function(BuildContext)> routes = {
-    Routes.homeScreen: (context) => const HomeScreen(),
+    "/": (context) => const HomeScreen(),
+    // Routes.homeScreen: (context) => const HomeScreen(),
     Routes.questionScreen: (context) => const QuestionScreen(),
     Routes.quizEndScreen: (context) => const QuizEndScreen(),
   };
